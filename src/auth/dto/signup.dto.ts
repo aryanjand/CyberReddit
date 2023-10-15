9efe2215bed5e3e0764f6b3aab3467aadfcbd93f
Oscar Zhu <ozhu35@gmail.com>
2023-10-14 18:41:24 -0700

    refactor(signupdto): include remaining columns

--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -1,8 +1,18 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString, IsUrl } from 'class-validator';
 import { SignInDto } from './signin.dto';
 
 export class SignUpDto extends SignInDto {
   @IsString()
   @IsNotEmpty()
   username: string;
+
+  @IsString()
+  firstName: string;
+
+  @IsString()
+  lastName: string;
+
+  @IsUrl()
+  @IsOptional()
+  profilePicture: string;
 }
