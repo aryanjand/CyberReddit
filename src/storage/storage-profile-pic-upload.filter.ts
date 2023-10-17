56f2aed6af1046d6b276ea886ab9a9fafa7f5afd
Oscar Zhu <ozhu35@gmail.com>
2023-10-17 00:04:17 -0700

    refactor(storage): include response status

--- src/storage/storage-profile-pic-upload.filter.ts
+++ src/storage/storage-profile-pic-upload.filter.ts
@@ -15,6 +15,7 @@ export class ProfilePicExceptionFilter implements ExceptionFilter {
     const request = ctx.getRequest<Request>();
     const session = request.session as UserSession;
 
+    response.status(exception.getStatus());
     return response.render('profile', {
       user: session.user,
       errors: exception.getResponse()['message'],
