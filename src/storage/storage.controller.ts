8ab7ee43761645e8feea2e12d3d239a11ae3fe71
Oscar Zhu <ozhu35@gmail.com>
2023-10-16 22:07:48 -0700

    refactor(storage): update session user after successful upload

--- src/storage/storage.controller.ts
+++ src/storage/storage.controller.ts
@@ -4,24 +4,31 @@ import {
   HttpStatus,
   ParseFilePipeBuilder,
   Post,
+  Response as Res,
   Session,
   UploadedFile,
   UseGuards,
   UseInterceptors,
+  UseFilters,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { Response } from 'express';
 import { AuthGuard, UserSession } from '../common';
+import { ProfilePicExceptionFilter } from './storage-profile-pic-upload.filter';
 import { StorageService } from './storage.service';
 
 @UseGuards(AuthGuard)
 @Controller('storage')
 export class StorageController {
   constructor(private storageService: StorageService) {}
+
   @HttpCode(HttpStatus.OK)
+  @UseFilters(ProfilePicExceptionFilter)
   @UseInterceptors(FileInterceptor('file'))
   @Post('avatars/upload')
-  uploadFile(
+  async uploadFile(
     @Session() session: UserSession,
+    @Res() res: Response,
     @UploadedFile(
       new ParseFilePipeBuilder()
         .addFileTypeValidator({ fileType: 'image' })
@@ -29,8 +36,8 @@ export class StorageController {
     )
     file: Express.Multer.File,
   ) {
-    console.log({ session, file });
-
-    return this.storageService.uploadAvatar(session.user.id, file);
+    const user = await this.storageService.uploadAvatar(session.user.id, file);
+    session.user = user;
+    res.redirect('/profile');
   }
 }
