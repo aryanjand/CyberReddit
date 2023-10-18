8eb4a43560230f06374c8f28e08815a559e8ae60
oscar <97479266+o-sca@users.noreply.github.com>
2023-10-17 17:09:02 -0700

    refactor(cloudinary): create rootpath to differ dev and prod avatar folders

--- src/cloudinary/cloudinary.service.ts
+++ src/cloudinary/cloudinary.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { v2 as cloudinary } from 'cloudinary';
 
 @Injectable()
 export class CloudinaryService {
+  constructor(private config: ConfigService) {}
+
   /**
    * Uploads a file to Cloudinary
    * @param {string} buf - The file buffer
@@ -10,11 +13,15 @@ export class CloudinaryService {
    * @param {number} id - The user id
    */
   async uploadFile(buf: string, fileId: string, id: number) {
+    const rootPath =
+      this.config.getOrThrow('NODE_ENV', 'development') === 'production'
+        ? 'production'
+        : 'development';
     const response = await cloudinary.uploader.upload(
       `data:image/png;base64,${buf}`,
       {
         resource_type: 'image',
-        public_id: `avatars/${id}/${fileId}`,
+        public_id: `${rootPath}/avatars/${id}/${fileId}`,
       },
     );
     return response;
