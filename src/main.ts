008bebb3ef47a33483ce13f5a62161c25871b301
oscar <97479266+o-sca@users.noreply.github.com>
2023-10-18 16:09:00 -0700

    chore: set trust proxy for cookie

--- src/main.ts
+++ src/main.ts
@@ -50,6 +50,10 @@ async function bootstrap() {
     origin: '*',
   });
 
+  if (process.env.NODE_ENV === 'production') {
+    app.set('trust proxy', 1);
+  }
+
   app.use(
     session({
       secret: process.env.SESSION_SECRET,
