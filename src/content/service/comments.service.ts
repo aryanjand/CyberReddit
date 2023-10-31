965410224e188dbdfc8238f7b5849e418d21dfce
Oscar Zhu <ozhu35@gmail.com>
2023-10-30 23:17:03 -0700

    feat(comments): create comment

--- src/content/service/comments.service.ts
+++ src/content/service/comments.service.ts
@@ -1,10 +1,27 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import * as bcrypt from 'bcrypt';
-import { Response } from 'express';
+import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service';
+import { CreateCommentDto } from '../dto';
 
 @Injectable()
 export class CommentsService {
-  constructor(private prisma: PrismaService, private config: ConfigService) {}
+  constructor(private prisma: PrismaService) {}
+
+  async create(dto: CreateCommentDto) {
+    try {
+      const contentResult = await this.prisma.content.create({
+        data: {
+          content_description: dto.content_description,
+          owner_user_id: dto.user_id,
+          content_parent_id: dto.content_id,
+        },
+      });
+      await this.prisma.comment.create({
+        data: {
+          content_id: contentResult.id,
+        },
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
 }
