9b7a8b35ec421b278339c19b51d393c51782058b
aryan <aryan.jand@gmail.com>
2023-10-30 21:02:14 -0700

    Threads Updated Service file

--- src/content/service/threads.service.ts
+++ src/content/service/threads.service.ts
@@ -4,58 +4,99 @@ import * as bcrypt from 'bcrypt';
 import { Response } from 'express';
 import { PrismaService } from '../../prisma/prisma.service';
 import { UserSession, ValidationException } from '../../common';
-import { User, Thread } from '@prisma/client';
+import { User, Thread, Content } from '@prisma/client';
 
 @Injectable()
 export class ThreadsService {
   constructor(private prisma: PrismaService, private config: ConfigService) {}
 
   async findAllThreads() {
-    const threads = await this.prisma.thread.findMany();
-    if (!threads) {
-      throw new ValidationException('No Threads', HttpStatus.NOT_FOUND);
+    const content_threads = await this.prisma.content.findMany({
+      include: {
+        thread: true,
+      },
+    });
+    if (!content_threads) {
+      throw new ValidationException('No Threads Found', HttpStatus.NOT_FOUND);
     }
-    return threads;
+    return content_threads;
+  }
+
+  async findMyThreads(userId: number) {
+    const content_thread = await this.prisma.content.findMany({
+      where: { owner_user_id: userId },
+      include: { thread: true },
+    });
+    if (!content_thread) {
+      throw new ValidationException('No Threads Found', HttpStatus.NOT_FOUND);
+    }
+    return content_thread;
   }
 
   async findThread(id: number) {
-    const thread = await this.prisma.thread.findUnique({
+    const content_threads = await this.prisma.content.findUnique({
       where: { id },
+      include: {
+        thread: true,
+      },
     });
-    if (!thread) {
+    if (!content_threads) {
       throw new ValidationException('No Threads', HttpStatus.NOT_FOUND);
     }
-    return thread;
+    return content_threads;
   }
 
+  // Should we make these transactions
   async createThread(postData: any) {
+    const content = await this.prisma.content.create({
+      data: {
+        content_description: postData.content_description,
+        owner_user_id: postData.owner_user_id,
+        content_parent_id: null,
+      },
+    });
+    if (!content) {
+      throw new ValidationException('No Content', HttpStatus.NOT_FOUND);
+    }
     const thread = await this.prisma.thread.create({
-      data: postData,
+      data: {
+        title: postData.title,
+        content_id: content.id,
+      },
     });
     if (!thread) {
       throw new ValidationException('No Threads', HttpStatus.NOT_FOUND);
     }
-    return thread;
+    return;
   }
-
-  async patchThread(postData: any, id: number) {
-    const thread = await this.prisma.thread.update({
+  // Should we make these transactions
+  async patchThread(putData: any, id: number) {
+    const content = await this.prisma.content.update({
       where: { id },
-      data: postData,
+      data: { content_description: putData.content_description },
+    });
+
+    if (!content) {
+      throw new ValidationException('No Content', HttpStatus.NOT_FOUND);
+    }
+
+    const thread = await this.prisma.thread.updateMany({
+      where: { content_id: content.id },
+      data: { title: putData.title },
     });
 
     if (!thread) {
-      throw new ValidationException('No Threads', HttpStatus.NOT_FOUND);
+      throw new ValidationException('No Thread', HttpStatus.NOT_FOUND);
     }
-    return thread;
+    return;
   }
 
   async deleteThread(id: number) {
-    const thread = await this.prisma.thread.delete({
+    const content_threads = await this.prisma.content.delete({
       where: { id },
     });
 
-    if (!thread) {
+    if (!content_threads) {
       throw new ValidationException('No Threads', HttpStatus.NOT_FOUND);
     }
     return `Thread with ID ${id} was successful deleted`;
