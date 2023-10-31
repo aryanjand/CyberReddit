11f7d6e31e4fec5cef6b11acecced051e57065a4
Oscar Zhu <ozhu35@gmail.com>
2023-10-30 23:16:46 -0700

    refactor(comments): set guards

--- src/content/controller/comments.controller.ts
+++ src/content/controller/comments.controller.ts
@@ -1,23 +1,28 @@
-// comments.controller.ts
 import {
+  Body,
   Controller,
+  Delete,
   Get,
+  Param,
   Post,
   Put,
-  Delete,
-  Param,
-  Body,
+  UseGuards,
 } from '@nestjs/common';
+import { AuthGuard, Public } from 'src/common';
+import { CreateCommentDto } from '../dto';
 
+@UseGuards(AuthGuard)
 @Controller('comments')
 export class CommentsController {
   // Get all comments
+  @Public()
   @Get()
   findAll(): string {
     return 'Get all comments';
   }
 
   // Get a specific comment by ID
+  @Public()
   @Get(':id')
   findOne(@Param('id') id: string): string {
     return `Get comment with ID ${id}`;
@@ -25,7 +30,7 @@ export class CommentsController {
 
   // Create a new comment
   @Post()
-  create(@Body() commentData: any): string {
+  create(@Body() dto: CreateCommentDto): string {
     return 'Create a new comment';
   }
 
