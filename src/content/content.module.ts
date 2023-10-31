af1b761a75fc0d94c1806ae0a6bd320d5ddb7103
Oscar Zhu <ozhu35@gmail.com>
2023-10-30 23:15:46 -0700

    fix(content):  missing provider

--- src/content/content.module.ts
+++ src/content/content.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ThreadsController } from './controller/threads.controller';
 import { CommentsController } from './controller/comments.controller';
+import { ThreadsService } from './service/threads.service';
 
 @Module({
   controllers: [ThreadsController, CommentsController], // Include the controllers from other modules
+  providers: [ThreadsService],
 })
 export class ContentModule {}
