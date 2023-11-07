eb1d8b150393a452e2453e5b2cd8d79ef7c88e9b
oscar <97479266+o-sca@users.noreply.github.com>
2023-11-07 10:08:22 -0800

    fix(comments): thread owner can delete other user comments

--- src/content/service/comments.service.ts
+++ src/content/service/comments.service.ts
@@ -70,7 +70,7 @@ export class CommentsService {
       }
 
       if (
-        content.content_parent.owner_user.id !== session.user.id ||
+        content.content_parent.owner_user.id !== session.user.id &&
         content.owner_user_id !== session.user.id
       ) {
         throw new UnauthorizedException('Comment does not belong to user');
